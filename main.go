058a8288410707040c747172f6ad9f38d54667b3
William Nkosi <william.dev.acc@gmail.com>
2024-08-19 01:23:05 -0400

    Created the user endpoint to accept user data

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"malawi-ride-share-backend/models"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
+
 
 func main() {
 	r := gin.Default()
@@ -11,6 +15,15 @@ func main() {
 			"message": "pong",
 		})
 	})
+	r.POST("/user", registerUser)
 	r.Run() // listen and serve on 0.0.0.0:8080
 
+}
+
+func registerUser(c *gin.Context) {
+	user := models.User{}
+	if err := c.BindJSON(&user); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
 }
---
+++ models/user.go
@@ -0,0 +1,9 @@
+package models
+
+type User struct {
+	FirstName string
+	LastName string 
+	PhoneNumber string
+	Email string
+	Age int
+}
