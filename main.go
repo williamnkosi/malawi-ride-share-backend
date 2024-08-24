9af88425584649834050cd3c2986d7f983427f2b
William Nkosi <william.dev.acc@gmail.com>
2024-08-24 00:42:38 -0400

    Added code to connect to a database

--- main.go
+++ main.go
@@ -1,23 +1,46 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 	"malawi-ride-share-backend/models"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
 )
 
 
 func main() {
 	r := gin.Default()
+
+	initializeDataBase()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.POST("/user", registerUser)
-	r.Run() // listen and serve on 0.0.0.0:8080
+ 	r.POST("/user", registerUser)
+ 	r.Run() // listen and serve on 0.0.0.0:8080
 
+
+}
+
+func initializeDataBase() {
+	connStr := "postgres://postgres:password@localhost:5432"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Successfully connected to PostgreSQL!")
 }
 
 func registerUser(c *gin.Context) {
