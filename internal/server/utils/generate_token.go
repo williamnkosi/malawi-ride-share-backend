4cb691eb2c08c90d1e9f32d8e061ce34364b7a8a
Mandla William Nkosi <16968612+williamnkosi@users.noreply.github.com>
2024-10-24 17:05:22 -0400

    Restructure files folders (#4)
    * Restructured the folders and files
    
    * Restructured the folders and files
    
    * restured and added more to gitignore

---
+++ internal/server/utils/generate_token.go
@@ -0,0 +1,23 @@
+package ServerUtils
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
+
+var jwtKey = []byte("testing-the-new-key")
+
+func GenerateToken(id string, phoneNumber string, firstName string, lastName string) (string, error) {
+	expirationTime := time.Now().Add(2 * time.Hour)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"id":              id,
+		"phoneNumber":     phoneNumber,
+		"firstName":       firstName,
+		"lastName":        lastName,
+		"expiration-time": expirationTime,
+	})
+	tokenString, err := token.SignedString(jwtKey)
+
+	return tokenString, err
+}
